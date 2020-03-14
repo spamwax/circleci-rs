b5e07a0d782da79cf89aa43e44f9b2f3d4503733
Hamid Ghadyani <hamid@hamid.cc>
2020-03-14 03:03:56 -0400

    Cont. the shit.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::io::Write;
+use tempfile::Builder;
+
 fn main() {
     println!("running main: {}", calculate(40));
+    let mut tfp = Builder::new()
+        .prefix("circleci_temp")
+        .suffix(".txt")
+        .tempfile_in("/home/circleci")
+        .unwrap();
+    let x = calculate(2);
+    writeln!(tfp, "I calculated the answer to be: {}", x).unwrap();
 }
 
 #[allow(dead_code)]
