952f9f8d7d0a4ccb4d3431c2cdb4e2dc6c78dd5e
Hamid Ghadyani <hamid@hamid.cc>
2020-03-14 03:54:56 -0400

    don't use tempfile

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
+use std::fs::File;
 use std::io::Write;
 use tempfile::Builder;
 
 fn main() {
     println!("running main: {}", calculate(40));
-    let mut tfp = Builder::new()
+    let mut fp = File::create("/home/circleci/circleci_temp.txt").unwrap();
+    let tfp = Builder::new()
         .prefix("circleci_temp")
         .suffix(".txt")
-        .tempfile_in("/home/circleci")
+        .tempfile()
         .unwrap();
     let x = calculate(2);
-    writeln!(tfp, "I calculated the answer to be: {}", x).unwrap();
+    println!("fname is: {:?}", tfp.path());
+    writeln!(fp, "I calculated the answer to be: {}", x).unwrap();
 }
 
 #[allow(dead_code)]
